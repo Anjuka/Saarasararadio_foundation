bf840a1c5af32d6baee5087bba5a08b8d41e94cf
Anjuka <anjukako@gmail.com>
2019-08-11 07:23:42 +0530

    First Release

--- app/src/main/java/com/anjukakoralage/saarasaradio/MainActivity.java
+++ app/src/main/java/com/anjukakoralage/saarasaradio/MainActivity.java
@@ -2,12 +2,20 @@ package com.anjukakoralage.saarasaradio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView wV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        wV = (WebView) findViewById(R.id.wV);
+        wV.getSettings().setJavaScriptEnabled(true);
+        wV.loadUrl("http://www.saarasaradio.com");
+
     }
 }
