2f1693e5fb5651ce1da7894b9718536ed9740cc2
Anjuka <anjukako@gmail.com>
2019-08-11 06:47:18 +0530

    Splash Screen Done

--- app/src/main/java/com/anjukakoralage/saarasaradio/SplashScreen.java
+++ app/src/main/java/com/anjukakoralage/saarasaradio/SplashScreen.java
@@ -1,13 +1,47 @@
 package com.anjukakoralage.saarasaradio;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
 
+    private Handler mWaitHandler = new Handler();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        mWaitHandler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+
+                //The following code will execute after the 5 seconds.
+
+                try {
+
+                    //Go to next page i.e, start the next activity.
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+
+                    //Let's Finish Splash Activity since we don't want to show this when user press back button.
+                    finish();
+                } catch (Exception ignored) {
+                    ignored.printStackTrace();
+                }
+            }
+        }, 4000);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        //Remove all the callbacks otherwise navigation will execute even after activity is killed or closed.
+        mWaitHandler.removeCallbacksAndMessages(null);
     }
 }
